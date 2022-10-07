67970266ac50361c0c4432db92bb961f4a35202e
yodayeong <duekdud01@naver.com>
2022-10-07 15:02:55 +0900

    m8 style revise

--- pjt/urls.py
+++ pjt/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reviews/', include('movies.urls')),
+    path('', include('movies.urls')),
 ]
